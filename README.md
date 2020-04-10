0ffdb66016841fc5195d22ca9c08ea2c78846acf
Lukas Koebis <lukaskoebis@gmail.com>
2020-04-11 00:26:46 +0100

    fixes for my setup

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # pomodoro
 Pomodoro App for Mac Statusbar with Google Calendar Integration
+
+```
+python3 -m venv .env
+source .env/bin/activate
+pip install -r requirements.txt
+python pomodoro.py
+# click start session, enter name, when done click stop session
+# ctrl+\ to kill
+
+# pip freeze > requirements.txt
+```
+
--- pomodoro.py
+++ pomodoro.py
@@ -1,4 +1,3 @@
-from __future__ import print_function
 import rumps
 import pickle
 import os.path
@@ -7,8 +6,7 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from datetime import datetime
-from datetime import timedelta 
-
+from datetime import timedelta
 
 class AwesomeStatusBarApp(rumps.App):
     def __init__(self):
@@ -17,15 +15,15 @@ class AwesomeStatusBarApp(rumps.App):
         self.status = 'init' # none, sess, break, inter, init
         self.start_cur = datetime.now()
         self.init_goal = ""
-        self.sess_len = 25
+        self.sess_len = 20
         self.break_len = 5
         self.menu['End Session'].set_callback(None)
         self.menu['Start Session'].set_callback(self.start_click)
 
     def start_new_sess(self):
         self.status = "inter"
-        test = rumps.Window(message="", title='Start Pomodoro Session', default_text='use mouse clicks to copy paste', ok="Start", cancel="Procrastinate", dimensions=(320,80))
-        test.icon = "/Users/clemens/Documents/Misc/pomodoro/invisible.png"
+        test = rumps.Window(message="What's your goal?", title='Start Pomodoro Session', default_text='', ok="Start", cancel="Procrastinate", dimensions=(320,80))
+        test.icon = "./invisible.png"
         response = test.run()
         if response.clicked == 1:
             self.init_goal = response.text
@@ -40,14 +38,14 @@ class AwesomeStatusBarApp(rumps.App):
             self.status = "none"
             self.menu['End Session'].set_callback(None)
             self.menu['Start Session'].set_callback(self.start_click)
-        
+
 
     def end_sess(self):
         end_time = datetime.now()
         self.status = "inter"
         self.title = "--:--"
-        test = rumps.Window(message="Goal: "+self.init_goal, title='Session Done!', default_text='use mouse clicks to copy paste', ok="Start Break", cancel="Finish", dimensions=(320,80))
-        test.icon = "/Users/clemens/Documents/Misc/pomodoro/invisible.png"
+        test = rumps.Window(message="Goal: "+self.init_goal+"\nDid you achieve your goal?", title='Session Done!', default_text='', ok="Start Break", cancel="Finish", dimensions=(320,80))
+        test.icon = "./invisible.png"
         response = test.run()
         # log the session
         self.g_test(self.start_cur, end_time, self.init_goal, response.text)
@@ -68,7 +66,7 @@ class AwesomeStatusBarApp(rumps.App):
         sec_str = (  str(60 - time_passed%60) if time_passed%60 != 0 else "00" )
         min_str = str(time_l - math.ceil(time_passed/60))
         return (min_str if len(min_str) == 2 else '0' + min_str ) + ":" + (sec_str if len(sec_str) == 2 else '0' + sec_str )
-    
+
 
     #@rumps.clicked("Start Session")
     def start_click(self, sender):
@@ -86,7 +84,7 @@ class AwesomeStatusBarApp(rumps.App):
         else:
             pass
 
-    @rumps.timer(1)  # create a new thread that calls the decorated function every 4 seconds
+    @rumps.timer(1)  # create a new thread that calls the decorated function every second
     def update_time(self, _):
         if self.status == "none":
             pass
@@ -125,7 +123,7 @@ class AwesomeStatusBarApp(rumps.App):
                 creds.refresh(Request())
             else:
                 flow = InstalledAppFlow.from_client_secrets_file(
-                    '/Users/clemens/Documents/Misc/credentials.json', ['https://www.googleapis.com/auth/calendar'])
+                    './credentials.json', ['https://www.googleapis.com/auth/calendar'])
                 creds = flow.run_local_server(port=0)
             # Save the credentials for the next run
             with open('token.pickle', 'wb') as token:
@@ -136,28 +134,23 @@ class AwesomeStatusBarApp(rumps.App):
         # Call the Calendar API
         now = datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
 
-        
+
         # creat an event
         event = {
           'summary': title,
-          'location': 'Notre Dame',
           'description': 'Pomodoro Session\nGoal: '+title+'\nAchieved: '+outcome,
           'start': {
-            'dateTime':  start_t.strftime("%Y-%m-%dT%H:%M:%S-05:00"), # 
-            'timeZone': 'America/New_York',
+            'dateTime':  start_t.strftime("%Y-%m-%dT%H:%M:%S"),
+            'timeZone': 'Europe/London',
           },
           'end': {
-            'dateTime':  end_t.strftime("%Y-%m-%dT%H:%M:%S-05:00"), # '2019-12-03T17:00:00-07:00', #
-            'timeZone': 'America/New_York',
+            'dateTime':  end_t.strftime("%Y-%m-%dT%H:%M:%S"),
+            'timeZone': 'Europe/London',
           },
         }
 
         event = service.events().insert(calendarId='primary', body=event).execute()
         print('Event created: %s' % (event.get('htmlLink')))
-
-
-
-
 
 if __name__ == "__main__":
     AwesomeStatusBarApp().run()
